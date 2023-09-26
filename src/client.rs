bbde57ec6506cfea592689a6a4cc40989e57c3e1
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2023-09-26 13:15:07 +0200

    Initial commit

---
+++ src/client.rs
@@ -0,0 +1,114 @@
+use std::sync::{Arc, Mutex};
+
+use ambient_api::{
+    core::{
+        hierarchy::components::parent, messages::Frame, player::components::user_id,
+        rendering::components::color,
+    },
+    element::{use_entity_component, use_query},
+    prelude::*,
+};
+use packages::this::{
+    components::{balls_per_frame, input_timestamp, latency, local_latency},
+    messages::{ChangeBallRate, Input, ReportLatency, SpawnBalls},
+};
+
+const FRAMES_PER_INPUT: usize = 10;
+const FRAMES_PER_LATENCY_REPORT: usize = 60;
+const LATENCY_SMOOTHING: u32 = 4;
+
+#[main]
+pub fn main() {
+    let current_rate = Arc::new(Mutex::new(0));
+    query(balls_per_frame()).each_frame({
+        let current_rate = current_rate.clone();
+        move |results| {
+            if let Some((_, rate)) = results.first() {
+                *current_rate.lock().unwrap() = *rate;
+            }
+        }
+    });
+
+    entity::add_component(entity::resources(), local_latency(), Duration::ZERO);
+    let player_id = player::get_local();
+    let mut frame_no = 0;
+    Frame::subscribe(move |_| {
+        let (input, _) = input::get_delta();
+        if input.keys.contains(&KeyCode::Q) || input.keys.contains(&KeyCode::E) {
+            let mut rate = *current_rate.lock().unwrap();
+            if input.keys.contains(&KeyCode::Q) {
+                rate = rate.saturating_sub(1);
+            }
+            if input.keys.contains(&KeyCode::E) {
+                rate = rate.saturating_add(1);
+            }
+            ChangeBallRate { rate }.send_server_reliable();
+        }
+
+        if input.keys.contains(&KeyCode::R) {
+            SpawnBalls { count: 100 }.send_server_reliable();
+        }
+
+        if frame_no % FRAMES_PER_INPUT == 0 {
+            Input {
+                timestamp: game_time(),
+            }
+            .send_server_unreliable();
+        }
+
+        if frame_no % FRAMES_PER_LATENCY_REPORT == 0 {
+            ReportLatency {
+                latency: entity::get_component(entity::resources(), local_latency()).unwrap(),
+            }
+            .send_server_reliable();
+        }
+
+        frame_no += 1;
+    });
+
+    query((parent(), input_timestamp())).each_frame(move |entities| {
+        let now = game_time();
+        for (id, (parent_id, timestamp)) in entities {
+            if parent_id != player_id {
+                continue;
+            }
+            let current_latency = now - timestamp;
+            entity::mutate_component(entity::resources(), local_latency(), |latency| {
+                *latency =
+                    (*latency * (LATENCY_SMOOTHING - 1) + current_latency) / LATENCY_SMOOTHING;
+            });
+            entity::despawn(id);
+        }
+    });
+
+    Status.el().spawn_interactive();
+}
+
+#[element_component]
+fn Status(hooks: &mut Hooks) -> Element {
+    let mut elements = Vec::new();
+    let rate = use_entity_component(hooks, entity::synchronized_resources(), balls_per_frame())
+        .0
+        .unwrap_or_default();
+    elements.push(Text::el(format!("Balls per frame: {}", rate)));
+    let input_latency = use_entity_component(hooks, entity::resources(), local_latency())
+        .0
+        .unwrap();
+    elements.push(Text::el(format!("Input latency: {:?}", input_latency)));
+    let mut player_latencies = use_query(hooks, (user_id(), latency()));
+    player_latencies.sort_unstable();
+    let local_player_id = player::get_local();
+    elements.extend(
+        player_latencies
+            .into_iter()
+            .map(|(id, (user_id, latency))| {
+                let el = Text::el(format!("{} latency: {:?}", user_id, latency));
+                if id == local_player_id {
+                    el.with(color(), vec4(1., 1., 1., 1.))
+                } else {
+                    el
+                }
+            }),
+    );
+    FlowColumn::el(elements)
+}
---
+++ src/server.rs
@@ -0,0 +1,141 @@
+use ambient_api::{
+    core::{
+        hierarchy::components::parent,
+        messages::Frame,
+        model::components::model_from_url,
+        physics::components::{cube_collider, dynamic, plane_collider, sphere_collider},
+        player::components::is_player,
+        primitives::{
+            components::{cube, quad},
+            concepts::Sphere,
+        },
+        rendering::components::color,
+        transform::{
+            components::{scale, translation},
+            concepts::{Transformable, TransformableOptional},
+        },
+    },
+    entity::despawn,
+    prelude::*,
+    rand,
+};
+use packages::{
+    base_assets,
+    character_animation::components::basic_character_animations,
+    fps_controller::components::use_fps_controller,
+    this::{
+        assets,
+        components::{balls_per_frame, bouncy_created, input_timestamp, latency, balls_to_spawn},
+        messages::{ChangeBallRate, Input, ReportLatency, SpawnBalls},
+    },
+};
+
+fn spawn_ball(with_ttl: bool) {
+    let mut ball = Entity::new()
+        .with_merge(Sphere::suggested())
+        .with_merge(Transformable::suggested())
+        .with(scale(), Vec3::ONE * 0.2)
+        .with(
+            translation(),
+            Vec3::X * 10. + (rand::random::<Vec2>() * 2.0 - 1.0).extend(10.),
+        )
+        .with(sphere_collider(), 0.5)
+        .with(dynamic(), true);
+    if with_ttl {
+        ball = ball.with(bouncy_created(), game_time())
+    }
+    ball.spawn();
+}
+
+#[main]
+pub fn main() {
+    Entity::new()
+        .with(quad(), ())
+        .with(scale(), Vec3::ONE * 10.0)
+        .with(color(), vec4(1.0, 0.0, 0.0, 1.0))
+        .with(plane_collider(), ())
+        .spawn();
+
+    spawn_query(is_player()).bind(move |players| {
+        for (id, _) in players {
+            entity::add_components(
+                id,
+                Entity::new()
+                    .with(use_fps_controller(), ())
+                    .with(model_from_url(), base_assets::assets::url("Y Bot.fbx"))
+                    .with(basic_character_animations(), id)
+                    .with(latency(), Duration::MAX),
+            );
+        }
+    });
+
+    for _ in 0..30 {
+        Entity::new()
+            .with(cube(), ())
+            .with(cube_collider(), Vec3::ONE * 0.5)
+            .with(
+                translation(),
+                (rand::random::<Vec2>() * 20.0 - 10.0).extend(1.),
+            )
+            .spawn();
+    }
+
+    entity::add_component(entity::synchronized_resources(), balls_per_frame(), 0);
+    entity::add_component(entity::resources(), balls_to_spawn(), 0);
+
+    Frame::subscribe(move |_| {
+        let rate = entity::get_component(entity::synchronized_resources(), balls_per_frame()).unwrap();
+        for _ in 0..rate {
+            spawn_ball(true);
+        }
+
+        if entity::get_component(entity::resources(), balls_to_spawn()).unwrap_or_default() > 0 {
+            entity::mutate_component(entity::resources(), balls_to_spawn(), |c| *c -= 1 );
+            spawn_ball(true);
+        }
+    });
+
+    query(bouncy_created()).each_frame(|entities| {
+        for (id, created) in entities {
+            if (game_time() - created).as_secs_f32() > 5.0 {
+                despawn(id);
+            }
+        }
+    });
+
+    ChangeBallRate::subscribe(move |_, msg| {
+        entity::set_component(entity::synchronized_resources(), balls_per_frame(), msg.rate);
+    });
+
+    Input::subscribe(|ctx, msg| {
+        let Some(player_id) = ctx.client_entity_id() else {
+            return;
+        };
+        Entity::new()
+            .with(parent(), player_id)
+            .with(input_timestamp(), msg.timestamp)
+            .spawn();
+    });
+
+    ReportLatency::subscribe(|ctx, msg| {
+        let Some(player_id) = ctx.client_entity_id() else {
+            return;
+        };
+        entity::set_component(player_id, latency(), msg.latency);
+    });
+
+    SpawnBalls::subscribe(|_, msg| {
+        entity::mutate_component(entity::resources(), balls_to_spawn(), |c| *c += msg.count );
+    });
+
+    // Entity::new()
+    //     .with_merge(Transformable {
+    //         local_to_world: Default::default(),
+    //         optional: TransformableOptional {
+    //             scale: Some(Vec3::ONE * 0.3),
+    //             ..Default::default()
+    //         },
+    //     })
+    //     .with(model_from_url(), assets::url("AntiqueCamera.glb"))
+    //     .spawn();
+}
